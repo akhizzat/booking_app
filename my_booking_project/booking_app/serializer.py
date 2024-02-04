6fe1fdda0bd53a469d9474f7af7780ab74910849
vasya <vasyaryzhov2906@gmail.com>
2024-02-04 16:19:33 +0300

    add requarments.txt

---
+++ my_booking_project/booking_app/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Room, Review, Booking, Payment
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
